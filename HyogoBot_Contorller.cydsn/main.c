07f275ddd3038b66aca580648edbd63850c743c1
Nozomu Araki <araki@eng.u-hyogo.ac.jp>
2019-06-01 12:01:40 +0900

    initial commit

---
+++ HyogoBot_Contorller.cydsn/main.c
@@ -0,0 +1,28 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    UART_Start();   // Serial Communication
+
+    for(;;)
+    {
+        /* Place your application code here. */
+        UART_PutString("This is PSoC 5LP Test\r\n");
+    }
+}
+
+/* [] END OF FILE */
